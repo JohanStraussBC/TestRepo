10de234ef9ae4d5889cdb78e56e67437d6367b6a
FIRST_NAME LAST_NAME <576341@student.belgiumcampus.ac.za>
2021-02-25 17:03:13 +0200

    First iteration

---
+++ DataAccessLayer/EventDAL.java
@@ -0,0 +1,17 @@
+package DataAccessLayer;
+import java.io.*;
+import java.util.*;
+import BusinessLayer.*;
+
+public class EventDAL 
+{
+    
+public List <Event> GetEvent()
+{
+
+List <Event> eveList = new ArrayList<Event>();
+
+
+}
+
+}
